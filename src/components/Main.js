f5c850480a835cfbdbec0da023d9095e117444c9
Manan Kevadiya <kevadiyamanan@gmail.com>
2021-02-20 18:58:41 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -130,28 +130,15 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action={JSONData.emailid} type="text/plain">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
+          <div className="field half first">
+            <label htmlFor="name">Phone:</label>
+            <p>+91 9427988692</p>
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email:</label>
+            <p>kevadiyamanan@gmail.com</p>
+          </div>
+
           <ul className="icons">
             <li>
               <a href={JSONData.twitter} className="icon fa-twitter">
